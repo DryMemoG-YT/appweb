8a451ec1ad6b21f288f7c9eb68353f32739f2597
Guillermo Galindo <86116574+profdCompu@users.noreply.github.com>
2021-07-16 08:45:31 -0600

    Creamos vistas, y las páginas donde se muestran las vistas

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+prestamos=[]
+
 @app.route('/')
-def hello_world():
-    return '<h1><center>Hola mundo</center></h1>'
+def index():
+    return render_template("index.html",num_prestamos=len(prestamos))
+    
+@app.route("/p/<string:slug>/")
+def show_prestamo(slug):
+    return render_template("prestamo_view.html",slug_title=slug)
+
+@app.route("/admin/prestamo/")
+@app.route("/admin/prestamo/<int:idPrestamo>")
+def prestamo_form(idPrestamo=None):
+    return render_template("admin/prestamo_form.html", idPrestamo=idPrestamo )
